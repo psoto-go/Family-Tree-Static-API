9cabfae1cbd7506347be828c2e307843920e2392
psoto-go <pabletesglez@gmail.com>
2021-07-26 08:32:51 +0000

    model

--- src/models.py
+++ src/models.py
@@ -2,14 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    lastName = db.Column(db.String(120), unique=False, nullable=False)
+    team = db.Column(db.String(120), unique=False, nullable=False),
+    age = db.Column(db.Integer, unique=False, nullable=False),
+    birthday = db.Column(db.String(20), unique=False, nullable=False)
+    generations = db.relationship('Generation', backref='member', lazy=True)
+
+    def __repr__(self):
+        return '<Member %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastName": self.lastName,
+            "team": self.team,
+            "age": self.name,
+            "birthday": self.birthday
+            # do not serialize the password, its a security breach
+        }
+
+class Generation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Generation %r>' % self.username
 
     def serialize(self):
         return {
